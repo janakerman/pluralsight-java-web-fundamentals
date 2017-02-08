7454a742afae439b42474f8fc50d85705c353085
Jan Akerman <jakerman@scottlogic.com>
2017-02-08 14:38:59 +0000

    Examples of application & servlet properties

--- src/main/java/com/pluralsight/SimpleServlet.java
+++ src/main/java/com/pluralsight/SimpleServlet.java
@@ -1,6 +1,7 @@
 package com.pluralsight;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -10,8 +11,20 @@ import java.io.IOException;
 /**
  * Created by jakerman on 08/02/2017.
  */
-@WebServlet(urlPatterns = {"/home", "*.do"})
+// We use init parameters here to provide default values.
+// When providing init parameters to our application, we need to do this in the web.xml.
+// These init parameters in the web.xml OVERRIDE the ones here.
+@WebServlet(urlPatterns = {"/home", "*.do"}, name = "SimpleServlet", initParams = {@WebInitParam(name = "ProductName", value = "Welcome Application")})
 public class SimpleServlet extends HttpServlet {
+
+    String appName;
+
+    @Override
+    public void init() throws ServletException {
+        // Taken from @WebServlet annotation - @WebInitParam
+        appName = getInitParameter("ProductName");
+        System.out.println(getServletContext().getInitParameter("jans-param"));
+    }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -21,9 +34,15 @@ public class SimpleServlet extends HttpServlet {
             resp.getWriter().write("Enter a name value");
             return;
         }
-        
+
         resp.setContentType("text/xml");
-        resp.getWriter().write("<name>" + name + "</name>");
+        StringBuilder sb = new StringBuilder();
+        sb.append("<application>");
+        sb.append("<name>" + name + "</name>");
+        sb.append("<product>" + appName + "</product>");
+        sb.append("</application>");
+
+        resp.getWriter().write(sb.toString());
     }
 
     @Override
